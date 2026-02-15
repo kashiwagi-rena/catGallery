d06916c888e942de5efec90abf22d5281d2dac2d
kashiwagi-rena <azisai07@gmail.com>
2026-02-15 23:20:34 +0900

    デプロイできろ！

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 
+	nitro: {
+		preset: 'deno'
+	},
+
+
 	modules: [],
 
 	css: ["~/assets/css/main.css"],
